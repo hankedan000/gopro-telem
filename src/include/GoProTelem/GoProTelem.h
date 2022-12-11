b84989e1c20aad3f238f3ea9a29ebe97d0621ae5
hankedan000 <dany32412@gmail.com>
2022-12-11 11:32:50 -0500

    Time stamps GPS samples based on average rate over whole MP4

--- src/include/GoProTelem/GoProTelem.h
+++ src/include/GoProTelem/GoProTelem.h
@@ -11,9 +11,23 @@
 namespace gpt
 {
 
+	/**
+	 * @param[in] pl
+	 * the payload to extract samples from
+	 * 
+	 * @param[in] timeOffset_sec
+	 * allows caller to offset the timestamps of the extracted samples.
+	 * 
+	 * @param[in] sampleRate_hz
+	 * allows caller to provide external rate for the GPS samples.
+	 * if negative, then sample rate will be computed based on the
+	 * payload's duration and sample count.
+	 */
 	std::vector<GPS_TimedSample>
 	getPayloadGPS_Samples(
-		GPMF_PayloadPtr pl);
+		GPMF_PayloadPtr pl,
+		double timeOffset_sec,
+		double sampleRate_hz = -1);
 
 	std::vector<GPS_TimedSample>
 	getGPS_Samples(
