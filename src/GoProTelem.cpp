a8daee1e585ff430b296100008c989566cfbfb3a
hankedan000 <dany32412@gmail.com>
2022-12-11 11:32:50 -0500

    Time stamps GPS samples based on average rate over whole MP4

--- src/GoProTelem.cpp
+++ src/GoProTelem.cpp
@@ -11,60 +11,69 @@ namespace gpt
 
 	std::vector<GPS_TimedSample>
 	getPayloadGPS_Samples(
-		GPMF_PayloadPtr pl)
+		GPMF_PayloadPtr pl,
+		double timeOffset_sec,
+		double sampleRate_hz)
 	{
 		std::vector<GPS_TimedSample> sampsOut;
 
-		double inTime = pl->inTime();
-		double outTime = pl->outTime();
 		auto stream = pl->getStream();
-
-		unsigned int sampIdx = 0;
-		while (stream->findNext(gpt::GPMF_KEY_GPS5, gpt::GPMF_RECURSE_LEVELS))
+		if ( ! stream->findNext(gpt::GPMF_KEY_GPS5, gpt::GPMF_RECURSE_LEVELS))
 		{
-			char* rawdata = (char*)stream->rawData();
-			FourCC key = stream->key();
-			char type = stream->type();
-			uint32_t samples = stream->repeat();
-			uint32_t elements = stream->elementsInStruct();
-			// printf("%d samples of type %c -- %d elements per samples\n", samples, type, elements);
-
-			if (samples == 0)
-			{
-				continue;
-			}
-			if (elements != 5)
-			{
-				throw std::runtime_error(
-					"invalid number of elements in GPS sample. expected 5. actual " + std::to_string(elements));
-			}
-
-			double samp_dt = (outTime - inTime) / samples;
-			size_t buffersize = samples * elements * sizeof(double);
-			double* ptr, * tmpbuffer = (double*)malloc(buffersize);
-
-			if ( ! stream->getScaledDataDoubles(tmpbuffer,buffersize,0,samples))
-			{
-				printf("FAILED TO READ GPS samples!\n");
-			}
-			else
-			{
-				sampsOut.resize(sampsOut.size() + samples);
-				for (unsigned int ss=0; ss<samples; ss++)
-				{
-					auto &sampOut = sampsOut.at(sampIdx);
-					sampOut.t_offset  = inTime + samp_dt * sampIdx;
-					sampOut.coord.lat = tmpbuffer[ss*elements+0];
-					sampOut.coord.lon = tmpbuffer[ss*elements+1];
-					sampOut.altitude  = tmpbuffer[ss*elements+2];
-					sampOut.speed2D   = tmpbuffer[ss*elements+3];
-					sampOut.speed3D   = tmpbuffer[ss*elements+4];
-					sampIdx++;
-				}
-			}
-
-			free(tmpbuffer);
+			// payload doesn't contain any GPS samples
+			return sampsOut;
 		}
+
+		char* rawdata = (char*)stream->rawData();
+		FourCC key = stream->key();
+		char type = stream->type();
+		uint32_t samples = stream->repeat();
+		uint32_t elements = stream->elementsInStruct();
+		// printf("%d samples of type %c -- %d elements per samples\n", samples, type, elements);
+
+		if (samples == 0)
+		{
+			return sampsOut;
+		}
+		if (elements != 5)
+		{
+			throw std::runtime_error(
+				"invalid number of elements in GPS sample. expected 5. actual " + std::to_string(elements));
+		}
+
+		double samp_dt = 0.0;
+		if (sampleRate_hz > 0.0)
+		{
+			samp_dt = 1.0 / sampleRate_hz;
+		}
+		else if (sampleRate_hz < 0.0)
+		{
+			// negative rate means compute based on payload duration & sample count
+			samp_dt = (pl->outTime() - pl->inTime()) / samples;
+		}
+
+		size_t buffersize = samples * elements * sizeof(double);
+		double* ptr, * tmpbuffer = (double*)malloc(buffersize);
+		if ( ! stream->getScaledDataDoubles(tmpbuffer,buffersize,0,samples))
+		{
+			printf("FAILED TO READ GPS samples!\n");
+		}
+		else
+		{
+			sampsOut.resize(sampsOut.size() + samples);
+			for (unsigned int ss=0; ss<samples; ss++)
+			{
+				auto &sampOut = sampsOut.at(ss);
+				sampOut.t_offset  = timeOffset_sec + samp_dt * ss;
+				sampOut.coord.lat = tmpbuffer[ss*elements+0];
+				sampOut.coord.lon = tmpbuffer[ss*elements+1];
+				sampOut.altitude  = tmpbuffer[ss*elements+2];
+				sampOut.speed2D   = tmpbuffer[ss*elements+3];
+				sampOut.speed3D   = tmpbuffer[ss*elements+4];
+			}
+		}
+
+		free(tmpbuffer);
 
 		return sampsOut;
 	}
@@ -75,13 +84,26 @@ namespace gpt
 	{
 		std::vector<GPS_TimedSample> sampsOut;
 
+		MP4_SensorInfo gpsInfo;
+		if ( ! mp4.getSensorInfo(GPMF_KEY_GPS5, gpsInfo))
+		{
+			return sampsOut;
+		}
+
+		double sampleRate_hz = gpsInfo.measuredRate_hz;
+		double samp_dt = 1.0 / sampleRate_hz;
+		double timeOffset_sec = 0.0;
 		size_t payloadCount = mp4.payloadCount();
 		for (size_t pIdx=0; pIdx<payloadCount; pIdx++)
 		{
 			// printf("pIdx = %ld\n", pIdx);
 			auto payloadPtr = mp4.getPayload(pIdx);
-			auto pSamps = getPayloadGPS_Samples(payloadPtr);
+			auto pSamps = getPayloadGPS_Samples(payloadPtr,timeOffset_sec,sampleRate_hz);
 			sampsOut.insert(sampsOut.end(), pSamps.begin(), pSamps.end());
+			if (pSamps.size() > 0)
+			{
+				timeOffset_sec = pSamps.back().t_offset + samp_dt;
+			}
 		}
 
 		return sampsOut;
